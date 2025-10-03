b2984f42a947d4bd230e473a1743696d6360eb2a
Tord Bärnfors <tord@barnfors.com>
2025-10-03 17:33:20 +0200

    C-Sharp wrapper: Currently needed functionality done.

--- src/wrappers/c-sharp/wg_softbackend.cs
+++ src/wrappers/c-sharp/wg_softbackend.cs
@@ -18,11 +18,42 @@ public class SoftBackend : GfxBackend
         return (wg_defineSoftBackendCanvas(_obj, reference, surface.CHandle()) == 1);
     }
 
+    //____ Static Add Kernel Methods _________________________________________________
+    
+    public static bool AddDefaultSoftKernels(SoftBackend backend) => wg_addDefaultSoftKernels(backend.CHandle()) == 1;
+    public static bool AddBaseSoftKernelsForBGR565LinearCanvas(SoftBackend backend) => wg_addBaseSoftKernelsForBGR565LinearCanvas(backend.CHandle()) == 1;
+    public static bool AddExtraSoftKernelsForBGR565LinearCanvas(SoftBackend backend) => wg_addExtraSoftKernelsForBGR565LinearCanvas(backend.CHandle()) == 1;
+    public static bool AddBaseSoftKernelsForBGR565sRGBCanvas(SoftBackend backend) => wg_addBaseSoftKernelsForBGR565sRGBCanvas(backend.CHandle()) == 1;
+    public static bool AddExtraSoftKernelsForBGR565sRGBCanvas(SoftBackend backend) => wg_addExtraSoftKernelsForBGR565sRGBCanvas(backend.CHandle()) == 1;
+    public static bool AddBaseSoftKernelsForRGB555BECanvas(SoftBackend backend) => wg_addBaseSoftKernelsForRGB555BECanvas(backend.CHandle()) == 1;
+
     //____ DLL functions ______________________________________________________
 
     [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
     private static extern IntPtr wg_createSoftBackend();
 
-	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
-    private static extern int wg_defineSoftBackendCanvas( IntPtr backend, CanvasRef canvas, IntPtr softSurface );
+    [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int wg_defineSoftBackendCanvas(IntPtr backend, CanvasRef canvas, IntPtr softSurface);
+
+
+
+    [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int wg_addDefaultSoftKernels(IntPtr backend);
+
+    [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int wg_addBaseSoftKernelsForBGR565LinearCanvas( IntPtr backend );
+
+    [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int wg_addExtraSoftKernelsForBGR565LinearCanvas( IntPtr backend );
+
+    [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int wg_addExtraSoftKernelsForBGR565sRGBCanvas( IntPtr backend );
+
+    [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int wg_addBaseSoftKernelsForRGB555BECanvas( IntPtr backend );
+
+    [DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int wg_addBaseSoftKernelsForBGR565sRGBCanvas( IntPtr backend );
+
+
 }
--- src/wrappers/c-sharp/wg_surface.cs
+++ src/wrappers/c-sharp/wg_surface.cs
@@ -4,26 +4,65 @@ using System.Runtime.InteropServices;
 namespace WG;
 
 
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct C_PixelBuffer
+	{
+		public PixelFormat format;
+		public IntPtr pixels;
+		public IntPtr palette;
+		public RectI rect;
+		public int pitch;
+	}
+
+//____ PixelBuffer _____________________________________________________________
+public unsafe struct PixelBuffer
+{
+	internal PixelBuffer(C_PixelBuffer c_buffer, int sizeInBytes )
+	{
+		this._c_buffer = c_buffer;
+
+		this.Format = c_buffer.format;
+		this.Rect = c_buffer.rect;
+		this.Pitch = c_buffer.pitch;
+		this._sizeInBytes = sizeInBytes;
+	}
+
+	public Span<T> Pixels<T>() where T : unmanaged
+	{
+		int count = _sizeInBytes / sizeof(T);
+		return new Span<T>(_c_buffer.pixels.ToPointer(), count);
+	}
+
+	public PixelFormat Format;
+	public RectI Rect;
+	public int Pitch;
+
+	internal C_PixelBuffer _c_buffer;
+	internal int _sizeInBytes;
+}
+
+
+//____ Surface ________________________________________________________________
 public class Surface : Objekt
 {
 	//____ Blueprint __________________________________________________________
 	public struct Blueprint
 	{
-		public Blueprint() {}
+		public Blueprint() { }
 
-		public bool 			Buffered = false;
-		public bool				Canvas = false;
-//		public const wg_color8* palette;				HOW DO WE DO THIS?
-		public int				PaletteSize = 0;
-		public int				PaletteCapacity = 0;
-		public bool				Dynamic = false;
-		public PixelFormat		format = PixelFormat.Undefined;
-		public int				Identity = 0;
-		public bool				Mipmap = false;
-		public SampleMethod		SampleMethod = SampleMethod.Undefined;
-		public int				Scale = 0;
-		public SizeI			Size;					// Mandatory, except when creating from other surface.
-		public bool				Tiling = false;
+		public bool Buffered = false;
+		public bool Canvas = false;
+		//		public const wg_color8* palette;				HOW DO WE DO THIS?
+		public int PaletteSize = 0;
+		public int PaletteCapacity = 0;
+		public bool Dynamic = false;
+		public PixelFormat format = PixelFormat.Undefined;
+		public int Identity = 0;
+		public bool Mipmap = false;
+		public SampleMethod SampleMethod = SampleMethod.Undefined;
+		public int Scale = 0;
+		public SizeI Size;                  // Mandatory, except when creating from other surface.
+		public bool Tiling = false;
 	}
 
 	//____ SetId() ____________________________________________________________
@@ -68,43 +107,89 @@ public class Surface : Objekt
 		return (wg_copySurfaceRect(_obj, dest, sourceSurface.CHandle(), ref sourceRect) == 1);
 	}
 
+    //____ GetPixelDescription() _________________________________________________
+    public ref readonly PixelDescription GetPixelDescription() => ref wg_surfacePixelDescription(_obj);
+
+    //____ GetPixelFormat() ______________________________________________________
+
+    public PixelFormat GetPixelFormat() => wg_surfacePixelFormat(_obj);
+
+    //____ GetPixelBits() ________________________________________________________
+    public int GetPixelBits() => wg_surfacePixelBits(_obj);
+
+    //____ IsOpaque() _________________________________________________________
+    public bool IsOpaque() => wg_surfaceIsOpaque(_obj) == 1;
+
+    //____ CanBeCanvas() ______________________________________________________
+    public bool CanBeCanvas() => wg_surfaceCanBeCanvas(_obj) == 1;
+
+	//____ AllocPixelBuffer() _________________________________________________
+	public PixelBuffer AllocPixelBuffer()
+	{
+		C_PixelBuffer c_buff = wg_allocPixelBuffer(_obj);
+
+		int sizeInBytes = (c_buff.rect.H - 1) * c_buff.pitch + (c_buff.rect.W * wg_surfacePixelBits(_obj)) / 8;
+
+		return new(c_buff,sizeInBytes);
+	}
+
+	//____ PushPixels() _______________________________________________________
+	public void PushPixels(in PixelBuffer buffer)
+	{
+		wg_pushPixels(_obj, buffer._c_buffer);
+	}
+
+	//____ PullPixels() _______________________________________________________
+	public void PullPixels(in PixelBuffer buffer)
+	{
+		wg_pullPixels(_obj, buffer._c_buffer);
+	}
+
+	//____ FreePixelBuffer() __________________________________________________
+	public void FreePixelBuffer(in PixelBuffer buffer)
+	{
+		wg_freePixelBuffer(_obj, buffer._c_buffer);
+	}
+
+	//____ C_Blueprint ________________________________________________________
 
 	[StructLayout(LayoutKind.Sequential)]
 	public struct C_Blueprint
 	{
-	    public byte					buffered;
-	    public byte					canvas;
-	    public IntPtr             	palette;
-		public int					paletteSize;
-	    public int					paletteCapacity;
-	    public byte					dynamic;
-	    public PixelFormat			format;
-	    public int					identity;
-	    public byte					mipmap;
-	    public SampleMethod			sampleMethod;
-	    public int					scale;
-	    public SizeI				size;                   // Mandatory, except when creating from other surface.
-	    public byte					tiling;
+		public byte buffered;
+		public byte canvas;
+		public IntPtr palette;
+		public int paletteSize;
+		public int paletteCapacity;
+		public byte dynamic;
+		public PixelFormat format;
+		public int identity;
+		public byte mipmap;
+		public SampleMethod sampleMethod;
+		public int scale;
+		public SizeI size;                   // Mandatory, except when creating from other surface.
+		public byte tiling;
 	}
 
-	static internal void ConvertBlueprint(in Blueprint org, out C_Blueprint converted )
+	//____ ConvertBlueprint() _________________________________________________
+	static internal void ConvertBlueprint(in Blueprint org, out C_Blueprint converted)
 	{
-		converted.buffered = (byte) (org.Buffered ? 1 : 0);
-		converted.canvas = (byte) (org.Canvas ? 1: 0);
-		converted.palette = 0;											//TODO: Need to solve this...
+		converted.buffered = (byte)(org.Buffered ? 1 : 0);
+		converted.canvas = (byte)(org.Canvas ? 1 : 0);
+		converted.palette = 0;                                          //TODO: Need to solve this...
 		converted.paletteSize = 0;
 		converted.paletteCapacity = org.PaletteCapacity;
-		converted.dynamic = (byte) (org.Dynamic ? 1: 0);
+		converted.dynamic = (byte)(org.Dynamic ? 1 : 0);
 		converted.format = org.format;
 		converted.identity = org.Identity;
-		converted.mipmap = (byte) (org.Mipmap ? 1 : 0);
+		converted.mipmap = (byte)(org.Mipmap ? 1 : 0);
 		converted.sampleMethod = org.SampleMethod;
 		converted.scale = org.Scale;
-		converted.size = new SizeI( org.Size.W, org.Size.H );
-		converted.tiling = (byte) (org.Tiling ? 1 : 0);
+		converted.size = new SizeI(org.Size.W, org.Size.H);
+		converted.tiling = (byte)(org.Tiling ? 1 : 0);
 	}
 
-    //____ DLL functions ______________________________________________________
+	//____ DLL functions ______________________________________________________
 
 	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
 	private static extern void wg_setSurfaceIdentity(IntPtr surface, int id);
@@ -127,7 +212,41 @@ public class Surface : Objekt
 	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
 	private static extern int wg_copySurfaceRect(IntPtr destSurface, CoordI dst, IntPtr sourceSurface, ref RectI srcRect);    ///< @brief Copy block of graphics from other surface
 
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern ref readonly PixelDescription wg_surfacePixelDescription(IntPtr surface); ///< @brief Get the pixel description for the surface.
 
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern PixelFormat wg_surfacePixelFormat(IntPtr surface);
+
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int wg_surfacePixelBits(IntPtr surface);
+
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int wg_surfaceIsOpaque(IntPtr surface);                ///< @brief Check if surface is guaranteed to be entirely opaque.
+
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int wg_surfaceCanBeCanvas(IntPtr surface);
+
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern C_PixelBuffer wg_allocPixelBuffer(IntPtr surface);
+
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern C_PixelBuffer wg_allocPixelBufferFromRect(IntPtr surface, in RectI rect);
+
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int wg_pushPixels(IntPtr surface, in C_PixelBuffer buffer);
+
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int wg_pushPixelsFromRect(IntPtr surface, in C_PixelBuffer buffer, in RectI bufferRect);
+
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern void wg_pullPixels(IntPtr surface, in C_PixelBuffer buffer);
+
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern void wg_pullPixelsFromRect(IntPtr surface, in C_PixelBuffer buffer, in RectI bufferRect);            // Needs to be overridden!
+
+	[DllImport(NativeLib, CallingConvention = CallingConvention.Cdecl)]
+	private static extern void wg_freePixelBuffer(IntPtr surface, in C_PixelBuffer buffer);
 
 
 }
