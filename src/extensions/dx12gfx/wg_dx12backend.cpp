c1403b86cb6c26adec69045f9e702453f2b0ffc8
Tord Bärnfors <tord@barnfors.com>
2025-12-03 16:59:11 +0100

    DX12 WIP: Start of rendering. Does not compile.

--- src/extensions/dx12gfx/wg_dx12backend.cpp
+++ src/extensions/dx12gfx/wg_dx12backend.cpp
@@ -26,6 +26,8 @@
 
 #include <wg_gfxbase.h>
 
+#include <D3dx12.h>
+
 namespace wg
 {
 
@@ -33,17 +35,37 @@ namespace wg
 
 	//____ create() ______________________________________________________________
 
-	DX12Backend_p DX12Backend::create()
+	DX12Backend_p DX12Backend::create(ID3D12Device* pDX12Device, ID3D12CommandQueue* pDX12CommandQueue)
 	{
-		return DX12Backend_p(new DX12Backend());
+		return DX12Backend_p(new DX12Backend(pDX12Device, pDX12CommandQueue));
 	}
 
 	//____ Constructor ___________________________________________________________
 
-	DX12Backend::DX12Backend()
+	DX12Backend::DX12Backend(ID3D12Device* pDX12Device, ID3D12CommandQueue* pDX12CommandQueue)
 	{
 		m_pSurfaceFactory = DX12SurfaceFactory::create();
 		m_pEdgemapFactory = DX12EdgemapFactory::create();
+
+		m_pDX12CommandQueue = pDX12CommandQueue;
+		m_pDX12Device = pDX12Device;
+
+		pDX12Device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_commandFence));
+		m_fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+
+		// Create command allocators for each frame resource
+
+		for (int i = 0; i < c_nbFrameResources; ++i)
+		{
+			pDX12Device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_frameResources[i].commandAllocator));
+			m_frameResources[i].fenceValue = 0;
+		}
+
+		// Create one command list for all frames (will be reset for each frame)
+
+		pDX12Device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_frameResources[0].commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList));
+
+		m_commandList->Close();
 	}
 
 	//____ Destructor ____________________________________________________________
@@ -63,18 +85,51 @@ namespace wg
 
 	void DX12Backend::beginRender()
 	{
+		int frame = m_currentFrameIndex;
+
+		_waitForFence(m_frameResources[frame].fenceValue);
+
+		m_frameResources[frame].commandAllocator->Reset();
+		m_commandList->Reset(m_frameResources[frame].commandAllocator.Get(), nullptr);
 	}
 
 	//____ endRender() _________________________________________________________
 
 	void DX12Backend::endRender()
 	{
+		m_commandList->Close();
+
+		// Execute the command list.
+		ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
+
+		 m_pDX12CommandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+	 
+		 // Signal and increment the fence value.
+
+		UINT64& fenceValue = ++m_fenceValues[m_currentFrameIndex];
+
+		m_pDX12CommandQueue->Signal(m_commandFence.Get(), fenceValue);
+		m_frameResources[m_currentFrameIndex].fenceValue = fenceValue;
+
+		// Update frame index.
+
+		m_currentFrameIndex = (m_currentFrameIndex + 1) % c_nbFrameResources;
 	}
 
 	//____ beginSession() _____________________________________________________
 
 	void DX12Backend::beginSession(CanvasRef canvasRef, Surface* pCanvas, int nUpdateRects, const RectSPX* pUpdateRects, const SessionInfo* pInfo)
 	{
+		CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(
+			backBuffers[currentBackBuffer].Get(),
+			D3D12_RESOURCE_STATE_PRESENT,
+			D3D12_RESOURCE_STATE_RENDER_TARGET
+		);
+
+
+		float clearColor[4] = { 0.2f, 0.3f, 0.4f, 1.0f };
+		m_commandList->ClearRenderTargetView(m_defaultCanvasRTV, clearColor, 0, nullptr);
+
 	}
 
 	//____ endSession() _______________________________________________________
@@ -97,24 +152,35 @@ namespace wg
 
 	void DX12Backend::setObjects(Object* const* pBeg, Object* const* pEnd)
 	{
+		m_pObjectsBeg = pBeg;
+		m_pObjectsEnd = pEnd;
+		m_pObjectsPtr = pBeg;
 	}
 
 	//____ setRects() __________________________________________________________
 
 	void DX12Backend::setRects(const RectSPX* pBeg, const RectSPX* pEnd)
 	{
+		m_pRectsBeg = pBeg;
+		m_pRectsEnd = pEnd;
+		m_pRectsPtr = pBeg;
 	}
 
 	//____ setColors() _________________________________________________________
 
 	void DX12Backend::setColors(const HiColor* pBeg, const HiColor* pEnd)
 	{
+		m_pColorsBeg = pBeg;
+		m_pColorsEnd = pEnd;
+		m_pColorsPtr = pBeg;
 	}
 
 	//____ setTransforms() _____________________________________________________
 
 	void DX12Backend::setTransforms(const Transform* pBeg, const Transform* pEnd)
 	{
+		m_pTransformsBeg = pBeg;
+		m_pTransformsEnd = pEnd;
 	}
 
 	//____ processCommands() ___________________________________________________
@@ -125,8 +191,9 @@ namespace wg
 
 	//____ setDefaultCanvas() ___________________________________________
 
-	bool DX12Backend::setDefaultCanvas(SizeSPX size, int scale)
+	bool DX12Backend::setDefaultCanvas(D3D12_CPU_DESCRIPTOR_HANDLE renderTargetView, SizeSPX size, int scale)
 	{
+		m_defaultCanvasRTV = renderTargetView;
 		m_defaultCanvas.ref = CanvasRef::Default;		// Starts as Undefined until this method is called.
 		m_defaultCanvas.size = size;
 		m_defaultCanvas.scale = scale;
@@ -185,6 +252,17 @@ namespace wg
 
 	void DX12Backend::waitForCompletion()
 	{
+	}
+
+	//____ _waitForFence() _____________________________________________________
+
+	void DX12Backend::_waitForFence(UINT64 fenceValue)
+	{
+		if (m_commandFence->GetCompletedValue() < fenceValue)
+		{
+			m_commandFence->SetEventOnCompletion(fenceValue, m_fenceEvent);
+			WaitForSingleObject(m_fenceEvent, INFINITE);
+		}
 	}
 
 }
